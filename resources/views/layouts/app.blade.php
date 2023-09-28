559d5124a52fa827a7f61e4ee05fd24b532fd0e5
br3-mah <nyeleti.bremah@gmail.com>
2023-09-28 11:11:00 +0200

    fixed route issue on products

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -215,7 +215,7 @@ License: For each use you must have a valid license purchased only from above li
 										<!--begin:Menu item-->
 										<div class="menu-item">
 											<!--begin:Menu link-->
-											<a class="menu-link" href=""{{ route('products') }}">
+											<a class="menu-link" href="{{ route('products') }}">
 												<span class="menu-bullet">
 													<span class="bullet bullet-dot"></span>
 												</span>
@@ -223,16 +223,6 @@ License: For each use you must have a valid license purchased only from above li
 											</a>
 											<!--end:Menu link-->
 										</div>
-										{{--  <div class="menu-item">
-											<!--begin:Menu link-->
-											<a class="menu-link" href="{{ route('my-products') }}">
-												<span class="menu-bullet">
-													<span class="bullet bullet-dot"></span>
-												</span>
-												<span class="menu-title">My Products</span>
-											</a>
-											<!--end:Menu link-->
-										</div>  --}}
 										<!--end:Menu item-->
 										
 									</div>
